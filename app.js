924cf9923e0eb3d8e7c3b633c73eef464b7206db
Arpiee <arpit.kohli01@gmail.com>
2018-08-11 09:20:40 -0400

    first commit

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// references we added
+const mongoose = require('mongoose');
+const config = require('./config/globals');
+
 var indexRouter = require('./controllers/index');
 var usersRouter = require('./controllers/users');
+const cars = require('./controllers/cars');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +27,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// map all requests with /cars to the cars controller
+app.use('/cars', cars);
+
+// db connection
+mongoose.connect(config.db);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
